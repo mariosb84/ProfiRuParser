2680818aa3372c45eb690068d03b22de1747129f
Sergei Baranov <mariosb84@mail.ru>
2025-06-20 11:23:36 +0300

    Build_20.06.2025

---
+++ src/main/java/org/example/profiruparser/ProfiRuParserApplication.java
@@ -0,0 +1,13 @@
+package org.example.profiruparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfiRuParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfiRuParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/profiruparser/ProfiRuParserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.profiruparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfiRuParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
